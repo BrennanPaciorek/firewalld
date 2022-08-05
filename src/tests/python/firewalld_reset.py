342a15df369f0f400a0527454bc390bde9ec950b
BrennanPaciorek <bpaciore@redhat.com>
2022-08-05 14:52:28 -0400

    add unit test file firewalld_reset.py

---
+++ src/tests/python/firewalld_reset.py
@@ -0,0 +1,10 @@
+import sys
+import unittest
+import firewall
+from firewall.client import FirewallClient, \
+                            FirewallClientZoneSettings, \
+                            FirewallClientServiceSettings, \
+                            FirewallClientIcmpTypeSettings
+from firewall.core.base import DEFAULT_ZONE_TARGET
+from firewall.core.fw import Firewall
+
